80acb42e284591d0fc6d93a135c1aafd57d5a960
William Luke <williamluke4@gmail.com>
2015-03-31 19:23:02 +0100

    Files Updated

---
+++ NodeSwitch/public/replacement/nav.js
@@ -0,0 +1,13 @@
+function page1() {	
+	$(".page").addClass("hidden");
+  	$("#page1").removeClass("hidden");
+  	$("li").removeClass("active");
+  	$("#page1btn").addClass("active");
+
+} 
+function page2() {	
+	$(".page").addClass("hidden");
+  	$("#page2").removeClass("hidden");
+  	$("li").removeClass("active");
+  	$("#page2btn").addClass("active");
+}
